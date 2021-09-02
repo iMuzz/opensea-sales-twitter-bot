5f677528e4524190bec72427e6d142086707e386
Faraaz Nishtar <faraaznishtar@gmail.com>
2021-09-02 05:51:04 -0400

    add image of text

--- app.js
+++ app.js
@@ -1,81 +1,105 @@
-const axios = require('axios');
-const _ = require('lodash');
-const moment = require('moment');
-const { ethers } = require('ethers');
-const tweet = require('./tweet');
-const cache = require('./cache');
+const axios = require("axios");
+const _ = require("lodash");
+const moment = require("moment");
+const { ethers } = require("ethers");
+const tweet = require("./tweet");
+const cache = require("./cache");
 
 // Format tweet text
 function formatAndSendTweet(event) {
-    // Handle individual items + bundles
-    const tokenName = _.get(event, ['asset', 'name'], _.get(event, ['asset_bundle', 'name']));
-    const openseaLink = _.get(event, ['asset', 'permalink'], _.get(event, ['asset_bundle', 'permalink']));
+  // Handle individual items + bundles
+  const tokenName = _.get(
+    event,
+    ["asset", "name"],
+    _.get(event, ["asset_bundle", "name"])
+  );
+  const openseaLink = _.get(
+    event,
+    ["asset", "permalink"],
+    _.get(event, ["asset_bundle", "permalink"])
+  );
 
-    const totalPrice = _.get(event, 'total_price');
-    const usdValue = _.get(event, ['payment_token', 'usd_price']);
-    const tokenSymbol = _.get(event, ['payment_token', 'symbol']);
+  const totalPrice = _.get(event, "total_price");
+  const usdValue = _.get(event, ["payment_token", "usd_price"]);
+  const tokenSymbol = _.get(event, ["payment_token", "symbol"]);
 
-    // OPTIONAL - if you want to tweet a status including the image too
-    // const imageUrl = _.get(event, ['asset', 'image_url']);
+  // OPTIONAL - if you want to tweet a status including the image too
+  const imageUrl = _.get(event, ["asset", "image_url"]);
 
-    const isEthSale = (tokenSymbol === 'WETH' || tokenSymbol === 'ETH');
-    const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
-    const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
+  const isEthSale = tokenSymbol === "WETH" || tokenSymbol === "ETH";
+  const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
+  const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
 
-    // OPTIONAL - don't tweet out sales below 1 ETH (preference, can be changed)
-    // if (Number(formattedEthPrice) < 1) {
-    //     console.log(`${tokenName} sold for ${formattedEthPrice}${ethers.constants.EtherSymbol}, below tweet price`);
-    //     return;
-    // }
+  // OPTIONAL - don't tweet out sales below 1 ETH (preference, can be changed)
+  // if (Number(formattedEthPrice) < 1) {
+  //     console.log(`${tokenName} sold for ${formattedEthPrice}${ethers.constants.EtherSymbol}, below tweet price`);
+  //     return;
+  // }
 
-    let tweetText;
+  let tweetText;
 
-    if (isEthSale) {
-        tweetText = `${tokenName} bought for ${formattedEthPrice}Ξ ($${formattedUsdPrice}) #NFT ${openseaLink}`;
-    } else {
-        tweetText = `${tokenName} bought for ${formattedUsdPrice} ${tokenSymbol} #NFT ${openseaLink}`;
-    }
+  if (isEthSale) {
+    tweetText = `
+    ${tokenName} bought for ${formattedEthPrice}Ξ ($${formattedUsdPrice})
+    ${openseaLink}
+    `;
+  } else {
+    tweetText = `
+    ${tokenName} bought for ${formattedUsdPrice} ${tokenSymbol}
+    ${openseaLink}
+    `;
+  }
 
-    console.log(tweetText);
+  console.log(tweetText);
 
-    // OPTIONAL - if you want the tweet to include an attached image
-    // return tweet.tweetWithImage(tweetText, imageUrl);
+  // OPTIONAL - if you want the tweet to include an attached image
+  // return tweet.tweetWithImage(tweetText, imageUrl);
 
-    return tweet.tweet(tweetText);
+  return tweet.tweetWithImage(tweetText, imageUrl);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
+  const lastSaleTime =
+    cache.get("lastSaleTime", null) ||
+    moment().startOf("minute").subtract(59, "seconds").unix();
 
-    console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
+  console.log(
+    `Last sale (in seconds since Unix epoch): ${cache.get(
+      "lastSaleTime",
+      null
+    )}`
+  );
 
-    axios.get('https://api.opensea.io/api/v1/events', {
-        params: {
-            collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
-            event_type: 'successful',
-            occurred_after: lastSaleTime,
-            only_opensea: 'false'
-        }
-    }).then((response) => {
-        const events = _.get(response, ['data', 'asset_events']);
+  axios
+    .get("https://api.opensea.io/api/v1/events", {
+      params: {
+        collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
+        event_type: "successful",
+        occurred_after: lastSaleTime,
+        only_opensea: "false",
+      },
+    })
+    .then((response) => {
+      const events = _.get(response, ["data", "asset_events"]);
 
-        const sortedEvents = _.sortBy(events, function(event) {
-            const created = _.get(event, 'created_date');
+      const sortedEvents = _.sortBy(events, function (event) {
+        const created = _.get(event, "created_date");
 
-            return new Date(created);
-        })
+        return new Date(created);
+      });
 
-        console.log(`${events.length} sales since the last one...`);
+      console.log(`${events.length} sales since the last one...`);
 
-        _.each(sortedEvents, (event) => {
-            const created = _.get(event, 'created_date');
+      _.each(sortedEvents, (event) => {
+        const created = _.get(event, "created_date");
 
-            cache.set('lastSaleTime', moment(created).unix());
+        cache.set("lastSaleTime", moment(created).unix());
 
-            return formatAndSendTweet(event);
-        });
-    }).catch((error) => {
-        console.error(error);
+        return formatAndSendTweet(event);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
 }, 60000);
